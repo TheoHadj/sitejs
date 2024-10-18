77efbbbdb4db85b2bf1edcf9192f6762644e39c4
Theo Hadj <theo.hadja@gmail.com>
2024-10-18 16:01:40 +0200

    add console.log

--- src/exercice/listeExercice/click/click.ts
+++ src/exercice/listeExercice/click/click.ts
@@ -7,8 +7,7 @@ canva.style.position ="relative";
 app?.appendChild(canva);
 
 canva.addEventListener('click', function(event) {
-    // console.log('X:', event.clientX, 'Y:', event.clientY);
-
+    console.log('X:', event.clientX, 'Y:', event.clientY);
     console.log(event)
     let img = document.createElement('img');
     img.src = '/src/assets/image.jpg';
